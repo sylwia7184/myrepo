0dd05316637bdd2fe7ad99567945bd47e214b546
maniak550 <5567812@gmail.com>
2024-06-03 21:23:03 +0200

    change how data works

--- src/main/java/com/example/database_display/entity/FirstTable.java
+++ src/main/java/com/example/database_display/entity/FirstTable.java
@@ -8,6 +8,31 @@ public class FirstTable {
     @Id
     private String id;
     private String name;
+    private String value;
 
-    // Getters and setters
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/example/database_display/entity/SecondTable.java
+++ src/main/java/com/example/database_display/entity/SecondTable.java
@@ -7,7 +7,32 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class SecondTable {
     @Id
     private String id;
+    private String title;
     private String description;
 
-    // Getters and setters
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
